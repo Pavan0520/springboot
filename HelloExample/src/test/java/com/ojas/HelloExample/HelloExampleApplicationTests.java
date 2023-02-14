6f1ae507259ccafb9af2e0d67b73a24d97a9ad0a
Pavan0520 <88772896+Pavan0520@users.noreply.github.com>
2023-02-14 14:34:10 +0530

    first commit

---
+++ HelloExample/src/test/java/com/ojas/HelloExample/HelloExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojas.HelloExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
